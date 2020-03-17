a9774c346503c568f33118854165bc691ab7928f
Tororoi <cantwell.tom@gmail.com>
2020-03-17 17:54:29 -0400

    Completed all deliverables

--- bake-off-challenge-frontend/index.js
+++ bake-off-challenge-frontend/index.js
@@ -2,6 +2,9 @@
 const bakesCont = document.querySelector('#bakes-container')
 const bakesDetail = document.querySelector('#detail')
 const newBakeForm = document.querySelector('#new-bake-form')
+const judgeBakes = document.querySelector('#judge-bake-button')
+bakesDetail.addEventListener("submit", handleRateSubmit)
+judgeBakes.addEventListener("click", handleJudging)
 //------Put bakes in sidebar------//
 function renderAllBakeNames(bakeList) {
     bakeList.forEach(renderOneBakeName)
@@ -27,7 +30,7 @@ function showBake(bake) {
     <img src=${bake.image_url} alt=${bake.name}>
     <h1>${bake.name}</h1>
     <p>${bake.description}</p>
-    <form id="score-form" data-id="1">
+    <form id="score-form" data-id="${bake.id}">
     <input value=${bake.score} type="number" name="score" min="0" max="10" step="1">
     <input type="submit" value="Rate">
     </form>
@@ -52,7 +55,54 @@ function handleBakeSubmit(event) {
     }
     postBake(newBake).then(renderOneBakeName)
 }
+//-------Rate Bake--------//
+function handleRateSubmit(event) {
+    event.preventDefault()
+    newRating = event.target
+    newScore = parseInt(newRating.childNodes[1].value)
+    fetchBake(newRating).then((bake)=> {
+        bake.score = newScore
+        return bake
+    })
+    .then(rateBake)
+}
 
+function rateBake(bake) {
+    //bake.id is undefined
+    return fetch(`http://localhost:3000/bakes/${bake.id}/ratings`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': "Bearer 699a9ff1-88ca-4d77-a26e-e4bc31cfc261"
+        },
+        body: JSON.stringify(bake)
+    })
+    .then(res => res.json())
+    .then(data => data)
+}
+//------Get Winner------//
+function handleJudging(event) {
+    judgeBtn = event.target
+    fetchWinner().then(updateWinnerClass)
+}
+function updateWinnerClass(bake) {
+     entryList = bakesCont.querySelectorAll(`[data-id]`)
+     entryList.forEach(entry => {
+        if (parseInt(entry.dataset.id) === bake.id) {
+            entry.className = "item winner"
+        }
+     })
+}
+function fetchWinner() {
+    return fetch(`http://localhost:3000/bakes/winner`, {
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(data => data)
+}
 //------Get All Bakes-----//
 function fetchBakes() {
     return fetch(`http://localhost:3000/bakes`, {
@@ -66,7 +116,6 @@ function fetchBakes() {
 }
 //------Get individual bakes------//
 function fetchBake(bake) {
-    //bake.id is undefined
     return fetch(`http://localhost:3000/bakes/${bake.dataset.id}`, {
         headers: {
             'Content-type': 'application/json',
